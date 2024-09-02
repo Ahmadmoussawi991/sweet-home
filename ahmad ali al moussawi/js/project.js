0633221c59530bde6edf20bae94bf531335596f0
Ahmadmoussawi991 <152263505+Ahmadmoussawi991@users.noreply.github.com>
2024-09-02 11:30:41 -0700

    Add files via upload

---
+++ ahmad ali al moussawi/js/project.js
@@ -0,0 +1,38 @@
+function houseClicks(){
+    window.localStorage.setItem('name',"House1");
+    window.localStorage.setItem("src","../images/house1.jpg")
+    window.localStorage.setItem('descreption',"Size:20x20<br><br>Price:80000$<br><br>Number of rooms:6<br><br>Takes Between 2-5 Months<br><br>");
+
+}
+function houseClicks2(){
+    window.localStorage.setItem('name',"House2");
+    window.localStorage.setItem("src","../images/house2.jpg")
+    window.localStorage.setItem('descreption',"Size:20x20 <br><br>Price:50000$ <br><br>Number of rooms:6<br><br>Takes Between 7-9 Months <br><br>");
+
+}
+function houseClicks3(){
+    window.localStorage.setItem('name',"House3");
+    window.localStorage.setItem("src","../images/house3.jpg")
+    window.localStorage.setItem('descreption',"Size:40x30<br><br>Price:100000$<br><br>Number of rooms:10<br><br>Takes Between 4-7 Months<br><br>");
+
+}
+function houseClicks4(){
+    window.localStorage.setItem('name',"House4");
+    window.localStorage.setItem("src","../images/house4.jpg")
+    window.localStorage.setItem('descreption',"Size:40x40<br><br>Price:150000$<br><br>Number of rooms:12<br><br>Takes Between 8-10 Months<br><br>");
+
+}
+function houseClicks5(){
+    window.localStorage.setItem('name',"House5");
+    window.localStorage.setItem("src","../images/house5.jpg")
+    window.localStorage.setItem('descreption',"Size:20x30<br><br>Price:75000$<br><br>Number of rooms:7<br><br>Takes Between 6-7 Months<br><br>");
+
+
+}
+function houseClicks6(){
+    window.localStorage.setItem('name',"House6");
+    window.localStorage.setItem("src","../images/house6.jpg")
+    window.localStorage.setItem('descreption',"Size:30x40<br><br>Price:120000$<br><br>Number of rooms:9<br><br>Takes Between 8-9 Months<br><br>");
+
+
+}
---
+++ ahmad ali al moussawi/js/room.js
@@ -0,0 +1,38 @@
+function RoomClicks(){
+    window.localStorage.setItem('name',"Room1");
+    window.localStorage.setItem("src","../images/room1.jpg")
+    window.localStorage.setItem('descreption',"Size:5x6<br><br>Price:10000$<br><br>Takes Between 2-5 Days<br><br>");
+
+}
+function RoomClicks2(){
+    window.localStorage.setItem('name',"Room2");
+    window.localStorage.setItem("src","../images/room2.jpg")
+    window.localStorage.setItem('descreption',"Size:3x4 <br><br>Price:5000$ <br><br>Takes Between 7-9 Days <br><br>");
+
+}
+function RoomClicks3(){
+    window.localStorage.setItem('name',"Room3");
+    window.localStorage.setItem("src","../images/room3.jpg")
+    window.localStorage.setItem('descreption',"Size:3x3<br><br>Price:12000$<br><br>Takes Between 4-7 Days<br><br>");
+
+}
+function RoomClicks4(){
+    window.localStorage.setItem('name',"Room4");
+    window.localStorage.setItem("src","../images/room4.jpg")
+    window.localStorage.setItem('descreption',"Size:6x4<br><br>Price:15000$<br><br>Takes Between 8-10 Days<br><br>");
+
+}
+function RoomClicks5(){
+    window.localStorage.setItem('name',"Room5");
+    window.localStorage.setItem("src","../images/room5.jpg")
+    window.localStorage.setItem('descreption',"Size:2x3<br><br>Price:7500$<br><br>Takes Between 6-7 Days<br><br>");
+
+
+}
+function RoomClicks6(){
+    window.localStorage.setItem('name',"Room6");
+    window.localStorage.setItem("src","../images/room6.jpg")
+    window.localStorage.setItem('descreption',"Size:6x7<br><br>Price:120000$<br><br>Takes Between 8-9 Days<br><br>");
+
+
+}
